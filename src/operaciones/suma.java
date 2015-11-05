6850b2d86ea59910775df22dc5eb91da9bdff815
keves <kevinpaulcalle@gmail.com>
2015-11-05 11:28:39 -0500

    hola MUNDO

---
+++ src/operaciones/suma.java
@@ -0,0 +1,20 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package operaciones;
+
+/**
+ *
+ * @author Cristian Torres
+ */
+public class suma {
+    public static void main(String[] args) {
+        float a,b,c;
+        a=5;
+        b=8;
+        c=a+b;
+        System.out.println(""+c);
+    }
+}
